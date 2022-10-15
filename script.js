7d15e9d094c90eb6677d021503bd4c81a9a4718f
Ludmilahttps <ludmila.https@gmail.com>
2022-10-15 16:05:22 -0300

    front_done

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function green()
-{
-    const aux = document.querySelector('.dish');
-    aux.classList.toggle("select");
+function selectDish(botao) {
+    priceDish = document.querySelector(".dish")
+    const aux = document.querySelector('.selectDish');
+    if (aux !== null) {
+        aux.classList.remove('selectDish');
+    }
+    botao.classList.add('selectDish');4
+}
+
+function selectDrink(botao) {
+    const aux = document.querySelector('.selectDrink');
+    if (aux !== null) {
+        aux.classList.remove('selectDrink');
+    }
+    botao.classList.add('selectDrink');
+}
+
+function selectDessert(botao) {
+    const aux = document.querySelector('.selectDessert');
+    if (aux !== null) {
+        aux.classList.remove('selectDessert');
+    }
+    botao.classList.add('selectDessert');
 }
