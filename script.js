0a01f00c6f696db5194f52f1efdf9aac9b612588
Ludmilahttps <ludmila.https@gmail.com>
2022-10-16 06:23:09 -0300

    done

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
-function selectDish(botao) 
-{
+function selectDish(botao) {
     const aux = document.querySelector('.selectDish');
     if (aux !== null) {
         aux.classList.remove('selectDish');
+        aux.querySelector('.icon').classList.add("disable");
+        if(botao == aux)
+        {
+            aux.classList.remove('selectDish');
+            aux.querySelector('.icon').classList.add("disable");
+            done();
+            return;
+        }
     }
     botao.classList.add('selectDish');
+    botao.querySelector('.icon').classList.remove("disable");
     priceDish = botao.querySelector(".price");
     done();
 }
 
-function selectDrink(botao) 
-{
+function selectDrink(botao) {
     const aux = document.querySelector('.selectDrink');
     if (aux !== null) {
         aux.classList.remove('selectDrink');
+        aux.querySelector('.icon').classList.add("disable");
+        if(botao == aux)
+        {
+            aux.classList.remove('selectDrink');
+            aux.querySelector('.icon').classList.add("disable");
+            done();
+            return;
+        }
     }
     botao.classList.add('selectDrink');
+    botao.querySelector('.icon').classList.remove("disable");
     priceDrink = botao.querySelector(".price");
     done();
 }
 
-function selectDessert(botao) 
-{
+function selectDessert(botao) {
     const aux = document.querySelector('.selectDessert');
     if (aux !== null) {
         aux.classList.remove('selectDessert');
+        aux.querySelector('.icon').classList.add("disable");
+        if(botao == aux)
+        {
+            aux.classList.remove('selectDessert');
+            aux.querySelector('.icon').classList.add("disable");
+            done();
+            return;
+        }
     }
     botao.classList.add('selectDessert');
+    botao.querySelector('.icon').classList.remove("disable");
     priceDessert = botao.querySelector(".price");
     done();
 }
@@ -35,19 +59,14 @@ function done()
 {
     let select = document.querySelectorAll(".selectDish").length + document.querySelectorAll(".selectDrink").length + document.querySelectorAll(".selectDessert").length;
 
-    const check = document.querySelector('.check');
-    const ncheck = document.querySelector('.ncheck');
-
     if (select == 3) {
-        check.classList.remove('check');
-        check.classList.add('ncheck');
-
-        ncheck.classList.add('check');
-        ncheck.classList.remove('ncheck');
+        document.querySelector(".finish").classList.add("enable");
+        document.querySelector(".undone").classList.add("disable");
+        document.querySelector(".done").classList.remove("disable");
     }
-}
-
-function finished()
-{
-    const check = document.querySelector('.check');
+    else {
+        document.querySelector(".finish").classList.remove("enable");
+        document.querySelector(".done").classList.add("disable");
+        document.querySelector(".undone").classList.remove("disable");
+    }
 }
