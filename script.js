9cf04fa2c37263f775da1dead2385a93e79e3323
Ludmilahttps <ludmila.https@gmail.com>
2022-10-17 23:44:40 -0300

    with bonus

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let price;
+
 function selectDish(botao) {
     const aux = document.querySelector('.selectDish');
     if (aux !== null) {
@@ -72,26 +74,69 @@ function whats(botao) {
     let aux2 = botao.querySelector(".done");
 
     if (aux2 != aux) {
-        //alert("Digite seu nome: ");
-        //alert("Digite seu endereço: ");
-
-        let name_dish = document.querySelector('.selectDish .name').innerHTML;
-        let name_drink = document.querySelector('.selectDrink .name').innerHTML;
-        let name_dessert = document.querySelector('.selectDessert .name').innerHTML;
 
         let price_dish = (document.querySelector('.selectDish .price').innerHTML).replace('R$ ', '').replace(',', '.');
         let price_drink = document.querySelector('.selectDrink .price').innerHTML.replace('R$ ', '').replace(',', '.');
         let price_dessert = document.querySelector('.selectDessert .price').innerHTML.replace('R$ ', '').replace(',', '.');
 
-        let price = Number(price_dish) + Number(price_drink) + Number(price_dessert);
+        price = Number(price_dish) + Number(price_drink) + Number(price_dessert);
 
-        let msg = `Olá, gostaria de fazer o pedido:
+        document.querySelector(".overlay").classList.remove("disable");
+        addOrder();
+    }
+}
+
+function sendZap() {
+
+    let name_dish = document.querySelector('.selectDish .name').innerHTML;
+    let name_drink = document.querySelector('.selectDrink .name').innerHTML;
+    let name_dessert = document.querySelector('.selectDessert .name').innerHTML;
+
+
+    const name = prompt("Digite seu nome: ");
+    const adress = prompt("Digite seu endereço: ");
+
+    let msg = `Olá, meu nome é ${name} e eu gostaria de fazer o pedido:
                     - Prato: ${name_dish}
                     - Bebida: ${name_drink}
                     - Sobremesa: ${name_dessert}
-                  Total: R$ ${price}`;
+                  Total: R$ ${price}
+                  Para ser entregue em: ${adress}`;
 
-        msgWhats = encodeURIComponent(msg);
-        window.open(`http://wa.me/55048991339151?text=${msgWhats}`);
-    }
+    msgWhats = encodeURIComponent(msg);
+    window.open(`http://wa.me/55048991339151?text=${msgWhats}`);
+
 }
+
+function cancelOrder() {
+    document.location.reload(true);
+}
+
+function addOrder() {
+    let name_dish = document.querySelector('.selectDish .name').innerHTML;
+    let name_drink = document.querySelector('.selectDrink .name').innerHTML;
+    let name_dessert = document.querySelector('.selectDessert .name').innerHTML;
+
+    let price_dish = (document.querySelector('.selectDish .price').innerHTML);
+    let price_drink = document.querySelector('.selectDrink .price').innerHTML;
+    let price_dessert = document.querySelector('.selectDessert .price').innerHTML;
+
+    let orderDishName = document.querySelector(".order-dish .name");
+    orderDishName.innerHTML = name_dish;
+    let orderDishPrice = document.querySelector(".order-dish .price");
+    orderDishPrice.innerHTML = price_dish;
+
+    let orderDrinkName = document.querySelector(".order-drink .name");
+    orderDrinkName.innerHTML = name_drink;
+    let orderDrinkPrice = document.querySelector(".order-drink .price");
+    orderDrinkPrice.innerHTML = price_drink;
+
+    let orderDessertName = document.querySelector(".order-dessert .name");
+    orderDessertName.innerHTML = name_dessert;
+    let orderDessertPrice = document.querySelector(".order-dessert .price");
+    orderDessertPrice.innerHTML = price_dessert;
+
+    let orderPrice = document.querySelector(".order-price .price");
+    orderPrice.innerHTML = `R$ ${price}`;
+}
+
