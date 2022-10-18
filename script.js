ec42955a2fa3046196b9243d69f2d89ba9a49d7b
Ludmilahttps <ludmila.https@gmail.com>
2022-10-17 21:12:44 -0300

    envia pro zap

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function selectDish(botao) {
     if (aux !== null) {
         aux.classList.remove('selectDish');
         aux.querySelector('.icon').classList.add("disable");
-        if(botao == aux)
-        {
+        if (botao == aux) {
             aux.classList.remove('selectDish');
             aux.querySelector('.icon').classList.add("disable");
             done();
@@ -22,8 +21,7 @@ function selectDrink(botao) {
     if (aux !== null) {
         aux.classList.remove('selectDrink');
         aux.querySelector('.icon').classList.add("disable");
-        if(botao == aux)
-        {
+        if (botao == aux) {
             aux.classList.remove('selectDrink');
             aux.querySelector('.icon').classList.add("disable");
             done();
@@ -41,8 +39,7 @@ function selectDessert(botao) {
     if (aux !== null) {
         aux.classList.remove('selectDessert');
         aux.querySelector('.icon').classList.add("disable");
-        if(botao == aux)
-        {
+        if (botao == aux) {
             aux.classList.remove('selectDessert');
             aux.querySelector('.icon').classList.add("disable");
             done();
@@ -55,8 +52,7 @@ function selectDessert(botao) {
     done();
 }
 
-function done() 
-{
+function done() {
     let select = document.querySelectorAll(".selectDish").length + document.querySelectorAll(".selectDrink").length + document.querySelectorAll(".selectDessert").length;
 
     if (select == 3) {
@@ -68,5 +64,34 @@ function done()
         document.querySelector(".finish").classList.remove("enable");
         document.querySelector(".done").classList.add("disable");
         document.querySelector(".undone").classList.remove("disable");
+    }
+}
+
+function whats(botao) {
+    let aux = botao.querySelector(".disable");
+    let aux2 = botao.querySelector(".done");
+
+    if (aux2 != aux) {
+        //alert("Digite seu nome: ");
+        //alert("Digite seu endereço: ");
+
+        let name_dish = document.querySelector('.selectDish .name').innerHTML;
+        let name_drink = document.querySelector('.selectDrink .name').innerHTML;
+        let name_dessert = document.querySelector('.selectDessert .name').innerHTML;
+
+        let price_dish = (document.querySelector('.selectDish .price').innerHTML).replace('R$ ', '').replace(',', '.');
+        let price_drink = document.querySelector('.selectDrink .price').innerHTML.replace('R$ ', '').replace(',', '.');
+        let price_dessert = document.querySelector('.selectDessert .price').innerHTML.replace('R$ ', '').replace(',', '.');
+
+        let price = Number(price_dish) + Number(price_drink) + Number(price_dessert);
+
+        let msg = `Olá, gostaria de fazer o pedido:
+                    - Prato: ${name_dish}
+                    - Bebida: ${name_drink}
+                    - Sobremesa: ${name_dessert}
+                  Total: R$ ${price}`;
+
+        msgWhats = encodeURIComponent(msg);
+        window.open(`http://wa.me/55048991339151?text=${msgWhats}`);
     }
 }
